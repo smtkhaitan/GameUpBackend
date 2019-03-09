f22054c2bf54c37032cb4cd390a5b7eac3ff9ca0
sumit.khaitan <sumit.khaitan@inmobi.com>
2019-03-09 21:35:39 +0530

    added login api

--- src/main/java/hackathon/controller/ApiController.java
+++ src/main/java/hackathon/controller/ApiController.java
@@ -3,11 +3,16 @@ package hackathon.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
+import com.google.gson.JsonObject;
+import hackathon.pojo.LoginDetails;
 import hackathon.pojo.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.mvel2.templates.TemplateRuntime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.object.SqlCall;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,10 +24,13 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 @RestController
 @Slf4j
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ApiController {
 
     @RequestMapping(value = {"/v1/gameUp/signUp"}, method = RequestMethod.POST)
@@ -41,18 +49,52 @@ public class ApiController {
         }
     }
 
+    @RequestMapping(value = {"/v1/gameUp/login"}, method = RequestMethod.POST)
+    public ResponseEntity<String> login(@RequestBody String loginInfoJson) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            LoginDetails loginDetails = mapper.readValue(loginInfoJson, LoginDetails.class);
+            if (StringUtils.isEmpty(loginDetails.getEmail()) || StringUtils.isEmpty(loginDetails.getPassword())) {
+                return new ResponseEntity<>("Email or Password Can't be Empty", HttpStatus.BAD_REQUEST);
+            }
+
+            //@todo:kalyani to make sure without lat long login is not called
+
+            Connection connection = getConnection();
+
+            //verify if user is present in the db.
+            String rawQuery = Resources.toString(Resources.getResource("verify.sql"), Charsets.UTF_8);
+            HashMap<String, String> contextMap = new HashMap<>();
+            contextMap.put("email", loginDetails.getEmail());
+            String parsedQuery = (String) TemplateRuntime.eval(rawQuery, contextMap);
+
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(parsedQuery);
+                while (resultSet.next()) {
+                    String dbPassword = resultSet.getString("password");
+                    if (!dbPassword.equals(loginDetails.getPassword())) {
+                        connection.close();
+                        return new ResponseEntity<>("Wrong Password", HttpStatus.UNAUTHORIZED);
+                    } else {
+                        UserInfo userInfo = getUserInfo(resultSet);
+                        JsonObject apiCallReply = getLoginResponse(userInfo);
+                        connection.close();
+                        return new ResponseEntity<>(apiCallReply.toString(), HttpStatus.ACCEPTED);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>("Failed Try Again", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Try Again", HttpStatus.BAD_REQUEST);
+    }
+
     public static boolean signUpDbInsert(HashMap contextMap) throws Exception {
 
-        // Connect to database
-        String url = "jdbc:sqlserver://codiecon.database.windows.net:1433;database=codeicon;user=satish@codiecon;" +
-                "password=Nitssats123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;" +
-                "loginTimeout=30;";
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(url);
-            String schema = connection.getSchema();
-            System.out.println("Successful connection - Schema: " + schema);
 
+        try {
+            Connection connection = getConnection();
             System.out.println("Query data example:");
             System.out.println("=========================================");
 
@@ -94,5 +136,31 @@ public class ApiController {
         String parsedQuery = (String) TemplateRuntime.eval(rawQuery, contextMap);
         ResultSet resultSet = statement.executeQuery(parsedQuery);
         return resultSet.next();
+    }
+
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:sqlserver://codiecon.database.windows.net:1433;database=codeicon;" +
+                "user=satish@codiecon;" +
+                "password=Nitssats123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;" +
+                "loginTimeout=30;";
+        Connection connection = null;
+        return DriverManager.getConnection(url);
+    }
+
+    private static UserInfo getUserInfo(ResultSet resultSet) throws SQLException {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setAge_grp(resultSet.getString("age_grp"));
+        userInfo.setDob(resultSet.getString("dob"));
+        userInfo.setEmail(resultSet.getString("email"));
+        userInfo.setGender(resultSet.getString("gender"));
+        userInfo.setIndoor(new ArrayList(Arrays.asList(resultSet.getString("indoor").split(","))));
+        userInfo.setOutdoor(new ArrayList(Arrays.asList(resultSet.getString("outdoor").split(","))));
+        userInfo.setName(resultSet.getString("name"));
+        userInfo.setPhone(resultSet.getString("phone"));
+        return userInfo;
+    }
+
+    private static JsonObject getLoginResponse(UserInfo userInfo) {
+        return new JsonObject();
     }
 }
